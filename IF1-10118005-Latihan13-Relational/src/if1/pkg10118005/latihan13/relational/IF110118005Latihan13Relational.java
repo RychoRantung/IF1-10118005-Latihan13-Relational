f3eebb6f439b67d660c9874a46014cb75f3c8af4
Rycho Rantung <55700409+RychoRantung@users.noreply.github.com>
2019-09-26 13:43:14 +0700

    Add files via upload

---
+++ IF1-10118005-Latihan13-Relational/src/if1/pkg10118005/latihan13/relational/IF110118005Latihan13Relational.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package if1.pkg10118005.latihan13.relational;
+
+/**
+ *
+ * @author USER
+ * NAMA   : Rycho Rantung
+ * KELAS  : IF-1
+ * NIM    : 10118005
+ * Deskripsi Program : Program ini berisi program yang menampilkan relasional
+ */
+public class IF110118005Latihan13Relational {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       int bil1 = 10;
+       int bil2 = 20;
+       System.out.println("a == b = " + (bil1==bil2));
+       System.out.println("a != b = " + (bil1!=bil2));
+       System.out.println("a > b = " + (bil1>bil2));
+       System.out.println("a < b = " + (bil1<bil2));
+       System.out.println("b >= a = " + (bil2>=bil1));
+       System.out.println("b <= a = " + (bil2<=bil1));
+    }
+    
+}
